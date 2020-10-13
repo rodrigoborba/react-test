73c41e30f0f5274476b1912972e9942e7dcd956f
rodrigo_borba <rodrigo.borba@jointecnologia.com.br>
2020-10-13 09:41:44 -0300

    uso basico estados

--- dist.js
+++ dist.js
@@ -14,15 +14,25 @@ function MeuComponente2(props) {
 }
 
 function MeuComponente3() {
+  const [telefone, setTelefone] = React.useState('51 3588-0101');
+  setTimeout(() => {
+    setTelefone('15 51 3588-0101');
+  }, 1000);
   return /*#__PURE__*/React.createElement("div", {
     className: "componente-3"
-  }, /*#__PURE__*/React.createElement(MeuComponente4, null));
+  }, /*#__PURE__*/React.createElement(MeuComponente4, {
+    telefone: telefone
+  }));
 }
 
-function MeuComponente4() {
+function MeuComponente4(props) {
+  const [idade, setIdade] = React.useState(28);
+  setTimeout(function () {
+    setIdade(39);
+  }, 1000);
   return /*#__PURE__*/React.createElement("div", {
     className: "componentes-4"
-  }, /*#__PURE__*/React.createElement("p", null, "Componente 4"));
+  }, /*#__PURE__*/React.createElement("p", null, "Componente 4  - ", idade, " - ", props.telefone));
 }
 
 function MeuComponente() {
--- main.js
+++ main.js
@@ -24,17 +24,31 @@ function MeuComponente2(props){
 }
 
 function MeuComponente3(){
+    const [telefone, setTelefone] = React.useState('51 3588-0101');
+
+    setTimeout(() => {
+        setTelefone('15 51 3588-0101');        
+    }, 1000);
+
     return (
         <div className="componente-3">
-            <MeuComponente4/>
+            <MeuComponente4 telefone={telefone}/>
         </div>
     )
 }
 
-function MeuComponente4(){
+function MeuComponente4(props){
+    const [idade, setIdade] = React.useState(28);
+    
+
+    setTimeout(function() {
+        setIdade(39);
+       
+    }, 1000)
+
     return (
         <div className="componentes-4">
-            <p>Componente 4</p>
+            <p>Componente 4  - {idade} - {props.telefone}</p>
         </div>
             
     )
@@ -46,6 +60,10 @@ function MeuComponente(){
             <MeuComponente1 />
         </div>
     )
+}
+
+function MeuComponenteIrmao(){
+    
 }
 
 ReactDOM.render(
