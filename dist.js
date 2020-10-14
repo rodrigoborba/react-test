daf0869eb3c86b1d95b2128df340a84b37293b18
rodrigo_borba <rodrigo.borba@jointecnologia.com.br>
2020-10-13 21:06:00 -0300

    teste estado 2 - NOK

--- dist.js
+++ dist.js
@@ -1,10 +1,12 @@
 const NomeContext = React.createContext('nome');
 
-function MeuComponente1() {
+function MeuComponente1(props) {
   const meuNome = 'Ayrton Teshima com Context API';
   return /*#__PURE__*/React.createElement("div", {
     className: "componente-1"
-  }, /*#__PURE__*/React.createElement(MeuComponente2, null, /*#__PURE__*/React.createElement("p", null, "teste")));
+  }, /*#__PURE__*/React.createElement(MeuComponente2, null, /*#__PURE__*/React.createElement(MeuComponente3, {
+    onClickIncrementar: props.onClickIncrementar
+  })));
 }
 
 function MeuComponente2(props) {
@@ -13,7 +15,7 @@ function MeuComponente2(props) {
   }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("header", null, props.children), /*#__PURE__*/React.createElement("footer", null)), /*#__PURE__*/React.createElement(MeuComponente3, null));
 }
 
-function MeuComponente3() {
+function MeuComponente3(props) {
   const [telefone, setTelefone] = React.useState('51 3588-0101');
   setTimeout(() => {
     setTelefone('15 51 3588-0101');
@@ -21,7 +23,8 @@ function MeuComponente3() {
   return /*#__PURE__*/React.createElement("div", {
     className: "componente-3"
   }, /*#__PURE__*/React.createElement(MeuComponente4, {
-    telefone: telefone
+    telefone: telefone,
+    onClickIncrementar: props.onClickIncrementar
   }));
 }
 
@@ -30,15 +33,59 @@ function MeuComponente4(props) {
   setTimeout(function () {
     setIdade(39);
   }, 1000);
+
+  function clickBotao() {
+    console.log('clicou 1');
+  }
+
   return /*#__PURE__*/React.createElement("div", {
     className: "componentes-4"
-  }, /*#__PURE__*/React.createElement("p", null, "Componente 4  - ", idade, " - ", props.telefone));
+  }, /*#__PURE__*/React.createElement("p", null, "Componente 4  - ", idade, " - ", props.telefone), /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    onClick: clickBotao
+  }, "Incrementar 1"), /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    onClick: () => {
+      console.log('Clicou 2 ');
+    }
+  }, "Incrementar 2"), /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    onClick: props.onClickIncrementar
+  }, "Incrementar 3"));
 }
 
-function MeuComponente() {
+function MeuComponente(props) {
   return /*#__PURE__*/React.createElement("div", {
     id: "componentes"
-  }, /*#__PURE__*/React.createElement(MeuComponente1, null));
+  }, /*#__PURE__*/React.createElement(MeuComponente1, {
+    onClickIncrementar: props.onClickIncrementar
+  }));
 }
 
-ReactDOM.render( /*#__PURE__*/React.createElement(MeuComponente, null), document.getElementById('app'));
+function MeuComponenteIrmao(props) {
+  return /*#__PURE__*/React.createElement("div", {
+    id: "componente-irmao"
+  }, /*#__PURE__*/React.createElement(MeuComponenteIrmao2, {
+    contador: props.contador
+  }));
+}
+
+function MeuComponenteIrmao2(props) {
+  return /*#__PURE__*/React.createElement("h2", null, "Contador: ", props.contador, " ");
+}
+
+function AppComponente() {
+  const [contador, incrementaContador] = React.useState(12);
+
+  const clickIncrementa = function () {
+    incrementaContador(contador + 1);
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(MeuComponente, {
+    onClickIncrementar: clickIncrementa
+  }), /*#__PURE__*/React.createElement(MeuComponenteIrmao, {
+    contador: contador
+  }));
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(AppComponente, null), document.getElementById('app'));
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const NomeContext = React.createContext('nome');
 
-function MeuComponente1(){
+function MeuComponente1(props){
     const meuNome = 'Ayrton Teshima com Context API';
     return (
         <div className="componente-1">
             <MeuComponente2>
-                <p>teste</p>
+                <MeuComponente3 onClickIncrementar={props.onClickIncrementar}/>
             </MeuComponente2>
         </div>
     )
@@ -23,7 +23,7 @@ function MeuComponente2(props){
     )
 }
 
-function MeuComponente3(){
+function MeuComponente3(props){
     const [telefone, setTelefone] = React.useState('51 3588-0101');
 
     setTimeout(() => {
@@ -32,7 +32,7 @@ function MeuComponente3(){
 
     return (
         <div className="componente-3">
-            <MeuComponente4 telefone={telefone}/>
+            <MeuComponente4 telefone={telefone} onClickIncrementar={props.onClickIncrementar}/>
         </div>
     )
 }
@@ -46,27 +46,62 @@ function MeuComponente4(props){
        
     }, 1000)
 
+    function clickBotao(){
+        console.log('clicou 1');
+    }
+
     return (
         <div className="componentes-4">
             <p>Componente 4  - {idade} - {props.telefone}</p>
+            <button type="button" onClick={clickBotao}>Incrementar 1</button>
+
+            <button type="button" onClick={() => {console.log('Clicou 2 ')}}>Incrementar 2</button>
+
+            <button type="button" onClick={props.onClickIncrementar}>Incrementar 3</button>
         </div>
             
     )
 }
 
-function MeuComponente(){
+function MeuComponente(props){
     return (
         <div id="componentes">
-            <MeuComponente1 />
+            <MeuComponente1 onClickIncrementar={props.onClickIncrementar}/>
         </div>
     )
 }
 
-function MeuComponenteIrmao(){
-    
+function MeuComponenteIrmao(props){
+    return (
+        <div id="componente-irmao">
+            <MeuComponenteIrmao2 contador={props.contador}/>
+        </div>
+    )
+}
+
+function MeuComponenteIrmao2(props){
+    return (
+        <h2>Contador: {props.contador} </h2>
+    )
+}
+
+function AppComponente(){
+
+    const [ contador, incrementaContador] = React.useState(1);
+
+    const clickIncrementa = function(){
+        incrementaContador(contador + 1)
+    }
+
+    return (
+        <React.Fragment>
+            <MeuComponente onClickIncrementar={clickIncrementa}/>
+            <MeuComponenteIrmao contador={contador}/>
+        </React.Fragment>
+    )
 }
 
 ReactDOM.render(
-    <MeuComponente/>,
+    <AppComponente/>,
     document.getElementById('app')
 )
